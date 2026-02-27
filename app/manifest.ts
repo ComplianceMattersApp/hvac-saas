ef47a8f5768833669cfd0c5074355f5795b0fcf2
Eddie Castellanos <eddie@compliancemattersca.com>
2026-02-26 18:06:04 -0800

    Add 192x192 icons for Android install

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,11 +10,8 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#0f1f35',
     theme_color: '#0f1f35',
     icons: [
-      {
-        src: '/icon.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
+  { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
+  { src: "/icon.png", sizes: "512x512", type: "image/png" },
       {
         src: '/apple-icon.png',
         sizes: '180x180',
