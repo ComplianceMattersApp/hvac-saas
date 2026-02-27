95609c8595e1d82402f6fcd2c9678e9b6babd135
Eddie Castellanos <eddie@compliancemattersca.com>
2026-02-26 17:21:10 -0800

    Force PWA service worker filename

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 const nextPWA = require("next-pwa")({
   dest: "public",
+  sw: "sw.js",          // <-- force the filename
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // keep any existing config options here
-};
+const nextConfig = {};
 
 module.exports = nextPWA(nextConfig);
