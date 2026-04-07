946460368bba48e45dca5b19efbc87d164a67bf8
Eddie Castellanos <eddie@compliancemattersca.com>
2026-04-07 00:25:20 -0700

    Remove PWA runtime for cloud-only retail launch

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
-const nextPWA = require("next-pwa")({
-  dest: "public",
-  sw: "sw.js",          // <-- force the filename
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextPWA(nextConfig);
+module.exports = nextConfig;
